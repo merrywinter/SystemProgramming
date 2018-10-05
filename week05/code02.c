6f5f40924ad6301dfee7d53b2767bbb3b8abcf79
mandooboo <jmlee3096@gmail.com>
2018-10-05 12:23:00 +0900

    'add'

---
+++ week05/code02.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/types.h>
+#include<dirent.h>
+#include<unistd.h>
+
+int main ()
+{
+	DIR *dp;
+	struct dirent *dentry;
+
+	if ((dp = opendir (".")) == NULL)
+		exit (1);
+
+	
+	while (dentry = readdir (dp))
+	{
+		if (dentry->d_ino != 0)
+			printf ("%s\n", dentry->d_name);
+	}
+
+	closedir (dp);
+	 return 0;
+}
---
+++ week05/code04.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<unistd.h>
+
+#define BUF_SIZE 256
+
+void printcwd ()
+{
+	char buffer[BUF_SIZE];
+
+	if (getcwd (buffer, BUF_SIZE) == NULL)
+		exit (1);
+
+	printf ("%s\n", buffer);
+}
+int main ()
+{
+	printcwd ();
+	chdir ("/usr/include");
+	printcwd ();
+	chdir ("..");
+	printcwd();
+	return 0;
+}
