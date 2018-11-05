d31a50d23ed0fad864f279111332d347577787d1
mandooboo <jmlee3096@gmail.com>
2018-11-05 22:25:56 +0900

    'j'

--- week10/code02_1.c
+++ week10/code02_1.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <unistd.h>
 
-void main ()
-{
-	printf ("Before executing ls -l \n");
-	execl ("/bin/ls", "ls", "-l", (char*)0);
-	printf ("After executing ls -l \n");
+int main(){
+	
+	printf("Before executing ls -l \n");
+	execl("/bin/ls", "ls", "-l", (char*)0);
+	printf("After executing ls -l \n");
+
+	return 0;
 }
