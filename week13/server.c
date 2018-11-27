4e8fffc1cee31780d0dfdac6f827606b0e9e2749
Ubuntu <ubuntu@ip-172-31-28-152.ap-northeast-2.compute.internal>
2018-11-27 07:56:01 +0000

    m

--- week13/server.c
+++ week13/server.c
@@ -9,66 +9,63 @@
 
 #define SIZE	sizeof (struct sockaddr_in)
 
-void closesock (int sig);
+void closesock(int sig);
 
 int sockfd_connect;
 
-void main ()
-{
+int main (){
+
 	int sockfd_listen;
 	char c;
 	struct sockaddr_in server = {AF_INET, htons(1234), INADDR_ANY};
 	struct sigaction act;
 
 	act.sa_handler = closesock;
-	sigfillset (&(act.sa_mask));
-	sigaction (SIGPIPE, &act, NULL);
+	sigfillset(&(act.sa_mask));
+	sigaction(SIGPIPE, &act, NULL);
 
 	/* ex13_02: socket open part */
-	if ((sockfd_listen = socket (AF_INET, SOCK_STREAM, 0)) == -1)
-	{
-		printf ("Socket Open Fail\n");
+	if((sockfd_listen = socket (AF_INET, SOCK_STREAM, 0)) == -1){
+		printf("Socket Open Fail\n");
 		exit(1);
 	}
+
 	/* ex13_03: socket bind part */
-	if (bind (sockfd_listen, (struct sockaddr *) &server, SIZE) == -1)
-	{
-		printf ("Bind Fail\n");
+	if(bind(sockfd_listen, (struct sockaddr *) &server, SIZE) == -1){
+		printf("Bind Fail\n");
 		exit(1);
 	}
+	
 	/* ex13_04: socket listen part */
-	if (listen (sockfd_listen, 5) == -1)
-	{
-		printf ("listen fail\n");
+	if(listen(sockfd_listen, 5) == -1){
+		printf("listen fail\n");
 		exit(1);
 	}
-	while (1)
-	{
+
+	while (1){
 		/* ex13_05: socket connect part */
-		if ((sockfd_connect = accept (sockfd_listen, NULL, NULL)) == -1)
-		{
-			printf ("Accept fail\n");
+		if((sockfd_connect = accept(sockfd_listen, NULL, NULL)) == -1){
+			printf("Accept fail\n");
 			continue;
 		}
 		
-		printf ("Request Accepted\n");
+		printf("Request Accepted\n");
 
-		while (recv (sockfd_connect, &c, 1, 0) > 0)
-		{
-			printf ("%c received\n", c);
-			send (sockfd_connect, &c, 1, 0);
-			
+		while(recv(sockfd_connect, &c, 1, 0) > 0){
+			printf("%c received\n", c);
+			send(sockfd_connect, &c, 1, 0);
 		}
 		
-		printf ("sockfd_connect closed\n");
-		close (sockfd_connect);
+		printf("sockfd_connect closed\n");
+		close(sockfd_connect);
 	}
+
+	return 0;
 }
 
-void closesock (int sig)
-{
-	close (sockfd_connect);
-	printf ("connection lost\n");
-	exit (1);
+void closesock(int sig){
+	close(sockfd_connect);
+	printf("connection lost\n");
+	exit(1);
 }
 
